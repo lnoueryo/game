5b5713aa9024b59c8515647825caf9fb92d64559
lnoueryo <jounetsism@gmail.com>
2022-10-14 01:25:43 +0900

    ミドルウェア変更

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -2,45 +2,7 @@ import type { Handle, RequestEvent, ResolveOptions } from '@sveltejs/kit'
 import { db } from '$lib/database'
 import { base } from '$app/paths'
 import type { MaybePromise } from '@sveltejs/kit/types/private';
-// /** @type {import('@sveltejs/kit').Handle} */
-// export const handle: Handle = async ({ event, resolve }) => {
-//   console.log('Hello')
-//   // get cookies from browser
-//   const session = event.cookies.get('session')
 
-//   if (!session) {
-//     // if there is no session load page as normal
-//     return await resolve(event)
-//   }
-
-//   // find the user based on the session
-//   const user = await db.user.findUnique({
-//     where: { userAuthToken: session },
-//     select: { username: true, role: true },
-//   })
-
-//   // if `user` exists set `events.local`
-//   if (user) {
-//     event.locals.user = {
-//       name: user.username,
-//       role: user.role.name,
-//     }
-//   }
-
-//   // load page as normal
-//   return await resolve(event)
-// }
-
-const NO_AUTH_PATH = [
-  '/login/',
-  '/register/'
-]
-let previousPage : string = base;
-
-const isUnneededAuth = (path: string) => {
-  if(!path.endsWith('/')) path += '/'
-  return NO_AUTH_PATH.includes(path)
-}
 
 
 
@@ -48,7 +10,6 @@ const next = async(event: RequestEvent<Partial<Record<string, string>>>, resolve
   let response;
   if(path) response = await redirect(path.replace('__data.js', ''))
   else response = await resolve(event)
-  previousPage = event.url.pathname.replace('__data.js', '')
   return response
 }
 
@@ -60,30 +21,11 @@ const redirect = (location: string, body?: string) => {
 }
 
 export const handle: Handle = async ({ event, resolve }) => {
-  console.log('middleware')
-  // if(event.url.pathname.includes('/__data.js')) return await next(event, resolve, previousPage)
-  // get cookies from browser
+
   const session = event.cookies.get('session')
   if(!session) return await next(event, resolve)
-  // console.log(event.request.method)
-  // if(session && isUnneededAuth(event.url.pathname.replace('__data.js', ''))) {
-  //   const path = previousPage == '/login' ? '/' : previousPage
-  //   return await next(event, resolve, path)
-  // }
-  
-  // if (!session && !isUnneededAuth(event.url.pathname.replace('__data.js', ''))) {
-  //   console.log(2)
-  //   return await next(event, resolve, '/login')
-  // }
-  
-  // if (!session && isUnneededAuth(event.url.pathname.replace('__data.js', ''))) {
-  //   console.log(3)
-  //   previousPage = event.url.pathname.replace('__data.js', '')
-  //   return await next(event, resolve)
-  // }
 
-  // find the user based on the session
-  const user = await db.user.findUnique({
+  const user = await db.players.findUnique({
     where: { userAuthToken: session },
     select: { username: true, role: true },
   })
